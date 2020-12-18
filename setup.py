2a42d5e28d5eec97c4cefb096cd09be34dc53f4a
jichaosong <jichaosong@outlook.com>
2020-12-18 14:20:09 +0800

    init

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+--------------------------------------
+    Author:     JiChao_Song
+    Date  :     2020/12/18 14:05
+    Desc  :
+--------------------------------------
+"""
+
+from setuptools import find_packages, setup
+setup(
+    name='ApiTestTool',
+    version='1.0.0',
+    description = '接口测试工具',
+    author = 'jichaosong',
+    author_email = 'jichaosong@outlook.com',
+    url='',
+    packages = ['autotest'],
+
+
+)
